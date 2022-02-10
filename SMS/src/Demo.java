bb09dc8a6758d5a012b24e44e43a0015c401d96e
Utku Yıldız <73200891+UtkuYldz@users.noreply.github.com>
2022-02-10 20:52:16 +0300

    Update Demo.java

--- SMS/src/Demo.java
+++ SMS/src/Demo.java
@@ -8,28 +8,28 @@ public class Demo {
 		Ogrenci ogrenci = new Ogrenci();
 		
 		
-		Danisman nihal = new Danisman("Nihal","Altuntaþ",20);
-		Danisman serkan = new Danisman("Serkan","Gönen",36);
-		Ogrenci utku = new Ogrenci("Utku","Yýldýz",20,200403713,3,3,5);
-		Ogrenci ugur = new Ogrenci("Uður","Kaya",60,200403709,3,5,1);
-		Ogrenci emirhan = new Ogrenci("Emirhan","Topal",18,200403629,5,10,3.4f);
+		Danisman nihal = new Danisman("Nihal","AltuntaÅŸ",20);
+		Danisman serkan = new Danisman("Serkan","GÃ¶nen",36);
+		Ogrenci utku = new Ogrenci("Utku","YÄ±ldÄ±z",20,123,3,3,5);
+		Ogrenci ugur = new Ogrenci("UÄŸur","Kaya",60,1234,3,5,1);
+		Ogrenci emirhan = new Ogrenci("Emirhan","Topal",18,12345,5,10,3.4f);
 		
 		danisman.addDanisman(nihal);
 		danisman.addDanisman(serkan);
 		
 		ogrenci.addOgrenci(utku);
-		utku.addDers("Nesne Yönelimli Programlama", "Zorunlu", 3, 2, 4, 100);
-		utku.addDers("Kariyer Planlama", "Seçmeli", 3, 2, 3, 100);
+		utku.addDers("Nesne YÃ¶nelimli Programlama", "Zorunlu", 3, 2, 4, 100);
+		utku.addDers("Kariyer Planlama", "SeÃ§meli", 3, 2, 3, 100);
 		utku.addDers("Diferansiyel Denklemler", "Zorunlu", 3, 2, 3, 100);
 
 		ogrenci.addOgrenci(ugur);
-		ugur.addDers("Nesne Yönelimli Programlama", "Zorunlu", 3, 2, 4,20);
-		ugur.addDers("Kariyer Planlama", "Seçmeli", 3, 2, 4, 30);
+		ugur.addDers("Nesne YÃ¶nelimli Programlama", "Zorunlu", 3, 2, 4,20);
+		ugur.addDers("Kariyer Planlama", "SeÃ§meli", 3, 2, 4, 30);
 		ugur.addDers("Diferansiyel Denklemler", "Zorunlu", 3, 2, 4, 40);
 		
 		ogrenci.addOgrenci(emirhan);
-		emirhan.addDers("Nesne Yönelimli Programlama", "Zorunlu", 3, 2, 4, 60);
-		emirhan.addDers("Kariyer Planlama", "Seçmeli", 3, 2, 4, 70);
+		emirhan.addDers("Nesne YÃ¶nelimli Programlama", "Zorunlu", 3, 2, 4, 60);
+		emirhan.addDers("Kariyer Planlama", "SeÃ§meli", 3, 2, 4, 70);
 		emirhan.addDers("Diferansiyel Denklemler", "Zorunlu", 3, 2, 4, 80);
 
 		
@@ -42,25 +42,25 @@ public class Demo {
 	//	nihal.Ogr_bilgi(emirhan);
 		
 		serkan.Ogr_isimdegis(emirhan, "Emirhan2");
-		serkan.Ogr_bilgi(emirhan);  	// Danýþman öðrencinin bilgilerine eriþebilir ancak deðiþtiremez.
+		serkan.Ogr_bilgi(emirhan);  	// DanÄ±ÅŸman Ã¶ÄŸrencinin bilgilerine eriÅŸebilir ancak deÄŸiÅŸtiremez.
 		nihal.Ogr_isimdegis(emirhan, "Emirhan3");
 		nihal.Ogr_bilgi(emirhan);
 		
-		serkan.Ogr_transkript(emirhan); // Öðrencinin Danýþmaný olmayan bir öðretmen öðrencinin transkriptine eriþemez.
+		serkan.Ogr_transkript(emirhan); // Ã–ÄŸrencinin DanÄ±ÅŸmanÄ± olmayan bir Ã¶ÄŸretmen Ã¶ÄŸrencinin transkriptine eriÅŸemez.
 		nihal.Ogr_transkript(emirhan);
 		
 		serkan.Ogr_transkript(utku);
 		serkan.Ogr_transkript(ugur);
 		
-		System.out.println("\nMevcut Danýþmanlar :");
+		System.out.println("\nMevcut DanÄ±ÅŸmanlar :");
 		danisman.printAllDanisman();
 		
-		System.out.println("\nSerkan hocanýn öðrencileri: \n");
+		System.out.println("\nSerkan hocanÄ±n Ã¶ÄŸrencileri: \n");
 		serkan.printAllOgrenci();
-		System.out.println("Nihal hocamýzýn öðrencileri: \n");
+		System.out.println("Nihal hocanÄ±n Ã¶ÄŸrencileri: \n");
 		nihal.printAllOgrenci();
 		
-		System.out.println("-----------------\nMevcut Öðrenciler: ");
+		System.out.println("-----------------\nMevcut Ã–ÄŸrenciler: ");
 		ogrenci.printAllOgrenci();
 		
 		
