ef116da4f4b0bc37f55b3addec2879a1cc002732
Utku Yıldız <73200891+UtkuYldz@users.noreply.github.com>
2022-02-10 20:47:55 +0300

    Add files via upload

---
+++ Odevv/src/Dersler.java
@@ -0,0 +1,27 @@
+
+public class Dersler {
+	public String dersIsmi;
+	public String zorunluSecmeli;
+	public int teorikDersSaati;
+	public int uygulamaliDersSaati;
+	public int krediSayisi;
+	/**
+	 * 
+	 * @param dersIsmi
+	 * @param zorunluSecmeli
+	 * @param teorikDersSaati
+	 * @param uygulamaliDersSaati
+	 * @param krediSayisi
+	 * @param notortalamasi
+	 */
+	
+	public Dersler(String dersIsmi, String zorunluSecmeli, int teorikDersSaati, int uygulamaliDersSaati, int krediSayisi , float notortalamasi)
+	{
+
+	}
+	
+		
+		
+	
+
+}
